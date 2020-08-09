a63db494bba394714fa0a0db088c372640bc5006
guizinhord <guizinhord@hotmail.com>
2020-08-09 15:45:18 -0300

    Nlw2

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+
+import styles from './styles'
+import AsyncStorage from '@react-native-community/async-storage';
+
+function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(res => {
+      if (res) {
+        const favoritedTeachers = JSON.parse(res);
+       
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
+    return(
+    <View style={styles.container}>
+        <PageHeader title="Meus Proffys favoritos"/>
+
+        <ScrollView
+        style={styles.lista}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 24,
+        }}
+      >
+        { favorites.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={true}
+            />
+          )
+        })}
+      </ScrollView>
+    </View>
+  )
+}
+
+export default Favorites;
---
+++ server/src/README.md
@@ -0,0 +1,14 @@
+# Funcionalidades 
+
+## Conexões
+
+- Rota para listar o total de conexoes realizadas;
+- Rota para criar uma nova conexão;
+
+## Aulas
+
+- Rota para cruar uma aula;
+- Rota listar aulas;
+    - Filtar por materia, dia da semana e horario;
+
+    
